197427497ee5175515ff8cddda977bbe3adb03c0
Lyubov Agulova <lu7623@gmail.com>
2024-03-15 10:47:13 +0300

    fix: fix agify form

--- vk-forms/src/components/AgifyForm/AgifyForm.tsx
+++ vk-forms/src/components/AgifyForm/AgifyForm.tsx
@@ -1,19 +1,17 @@
-import { Button, FormItem, Input } from "@vkontakte/vkui";
+import { Button, FormItem, FormLayoutGroup, Input } from "@vkontakte/vkui";
 import "@vkontakte/vkui/dist/vkui.css";
 
 import { useEffect, useReducer, useState } from "react";
 import getAgeByName from "../../api/agify";
 import { initialState, reducer } from "./reducer";
 
-
 export default function AgifyForm() {
   const [age, setAge] = useState(0);
   const [state, dispatch] = useReducer(reducer, initialState);
   const [repeat, setRepeat] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
-  const [isValid, setIsValid] = useState(false);
-  const [errors, setErrors]=useState('')
-
+  const [isValid, setIsValid] = useState(true);
+  const [errors, setErrors] = useState("");
 
   const getAge = async (name: string) => {
     const alreadyAsked = state.find((man) => man.name === name);
@@ -22,9 +20,10 @@ export default function AgifyForm() {
       setRepeat("");
       if (res) {
         setAge(res);
-
+        setErrors("");
         dispatch({ type: "addName", payload: { name: name, age: res } });
       } else {
+        setErrors("Something went wrong. Please try again");
       }
     } else {
       const res = alreadyAsked.age;
@@ -34,48 +33,54 @@ export default function AgifyForm() {
   };
 
   const validate = (str: string) => {
-    if (str.length===0) {
+    if (str.length === 0) {
       setIsValid(false);
       setErrors("Please enter a name");
-      return false
-    }
-    else if (!str.match(/^[a-zA-Z]+$/)) {
+      return false;
+    } else if (!str.match(/^[a-zA-Z]+$/)) {
       setIsValid(false);
       setErrors("Name should only contain letters");
-      return false
-    }
-    else {
+      return false;
+    } else {
       setIsValid(true);
-      setErrors('')
-      return true
+      setErrors("");
+      return true;
     }
-  }
-  
+  };
+
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
-    isValid &&  getAge(searchTerm);
-    }, 3000)
+      searchTerm.length>0 && isValid && getAge(searchTerm);
+    }, 3000);
 
-    return () => clearTimeout(delayDebounceFn)
-  }, [searchTerm])
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchTerm]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-   getAge(searchTerm) 
-  }
+    getAge(searchTerm);
+  };
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRepeat("");
+    validate(e.target.value);
+    setSearchTerm(e.target.value);
+  };
   return (
     <>
+      <FormLayoutGroup>
       <form onSubmit={handleSubmit}>
-        <FormItem>
+          <FormItem
+            status={isValid ? 'valid' : 'error'}
+          bottom={
+           !isValid && errors
+          }>
           <Input
-            onChange={(e) => {
-              validate(e.target.value)
-              setSearchTerm(e.target.value)
-            }} 
-            value={searchTerm} 
-            name='manName'
-            required
+            onChange={handleChange}
+            value={searchTerm}
+            name="manName"
+              required
+             
           />
         </FormItem>
         <FormItem>
@@ -87,15 +92,18 @@ export default function AgifyForm() {
           >
             Submit
           </Button>
-        </FormItem>
-      </form>
-      {errors.length > 0 && <p>{ errors}</p>}
+          </FormItem>
+         
+        </form>
+        </FormLayoutGroup>
       <div>
         {repeat.length > 0 && <p>{repeat}</p>}
         <p>
           Age: <span>{age}</span>{" "}
         </p>
-      </div>
-    </>
+        </div>
+      
+      </>
+      
   );
 }
